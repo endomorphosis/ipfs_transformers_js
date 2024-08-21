4da300d236fc77bfe509faaefc057a5256dab4af
endomorphosis <starworks5@gmail.com>
2024-08-21 04:47:16 -0700

    refactor: Update test.js to use testIpfsTransformersJs class name

--- tests/test.js
+++ tests/test.js
@@ -5,8 +5,27 @@ class testIpfsTransformersJs {
   constructor() {
   }
 
+  async init() {
+    let meta = {};
+    this.thisDir = path.dirname(import.meta.url);
+    if (this.thisDir.startsWith("file://")) {
+        this.thisDir = this.thisDir.replace("file://", "");
+    }
+    this.parentDir = path.dirname(this.thisDir);
+    if (fs.existsSync(path.join(this.parentDir, "config", "config.toml"))) {
+        this.config = new requireConfig({config: path.join(this.parentDir, "config", "config.toml")});
+    }
+    else{
+        // this.config = new requireConfig();
+    }
+    for (let key in this.config) {
+        this[key] = meta[key];
+    }
+    this.test = new ipfsTransformersJs(null, meta);
+    
+  }
+
   async test() {
-    const test = new ipfsTransformersJs();
     
     return await test.pipeline('text-classification', 'Xenova/distilbert-base-uncased-finetuned-sst-2-english', { progress_callback: null }).then((pipeline) => {
         pipeline.predict('This is a test').then((result) => {
@@ -22,6 +41,7 @@ export default testIpfsTransformersJs;
 if (require.main === module) {
   const test = new testPipeline();
   try{
+    test.init();
     test.test();
   } catch (e) {
     console.error(e);
