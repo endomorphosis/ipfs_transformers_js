72ce02b45566b12e85ea2b289c2f6b5ba0fec99b
endomorphosis <starworks5@gmail.com>
2024-09-04 15:11:12 -0700

    refactor: Update package.json, index.js, and test/test.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 export * from './ipfs_transformers_js/ipfs_transformers.js';
+export { ipfsTransformersJs } from './ipfs_transformers_js/ipfs_transformers.js';
+export { ipfsModelManager } from './ipfs_model_manager_js/ipfs_model_manager.js';
+export * from './test/test.js';
+export { testIpfsTransformersJs } from './test/test.js';
+const test = new testIpfsTransformersJs();
+export { test };
